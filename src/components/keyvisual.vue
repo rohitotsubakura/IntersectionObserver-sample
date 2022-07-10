f458597cdbb483cf854ed483d027df8cd4fbdfd9
Rohito Tsubakura <rohitotsubakura@gmail.com>
2022-07-11 08:10:41 +0900

    modify: ラフにKV作った

--- src/components/keyvisual.vue
+++ src/components/keyvisual.vue
@@ -1,33 +1,55 @@
 <template>
 <div class="m-keyvisual">
-    <img src="/kv-gals.png" class="gals"/>
+    <img src="@/assets/kv-gals.png" class="gals" />
+    <img src="@/assets/kv-text.svg" class="text" />
 </div>
 </template>
 <script setup lang="ts">
 
 </script>
 <style lang="scss">
-.m-keyvisual {
-    width: 100%;
-    height: 100vh;
-    background-image: url('/cyber-4.png');
-    background-position: center;
-    background-size: cover;
-    overflow: hidden;
-    > .gals {
-        width: 100%;
-        height: auto;
-        animation: fadeIn 0.5s ease-in-out forward;
-    }
-}
 @keyframes fadeIn {
     from {
         opacity: 0;
-        transform: translate3d(0, -10px, 0);
+        transform: translateY(20px);
     }
     to {
         opacity: 1;
-        transform: translate3d(0, 0, 0);
+        transform: translateY(0);
+    }
+}
+@keyframes push {
+    from {
+        opacity: 0;
+        transform: translateZ(20px);
+    }
+    to {
+        opacity: 1;
+        transform: translateZ(0);
+    }
+}
+.m-keyvisual {
+    width: 100%;
+    height: 100vh;
+    background-image: url('/cyber-4.jpg');
+    background-position: center;
+    background-size: cover;
+    overflow: hidden;
+    position: relative;
+    > .gals {
+        width: 100%;
+        height: auto;
+        animation: fadeIn 2s ease-in-out both;
+    }
+    > .text {
+        z-index: 2;
+        width: 80%;
+        position: absolute;
+        bottom: 12px;
+        left: 0;
+        right: 0;
+        margin: auto;
+        animation: push 1s ease-out both;
     }
 }
 </style>
