ac69733ddeeda5d3c01176ef1cdd9846affef51c
Rohito Tsubakura <rohitotsubakura@gmail.com>
2022-07-19 09:17:24 +0900

    add: push-dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    target: 'static',
+    router: {
+      base: '/IntersectionObserver-sample/'
+    }
 })
