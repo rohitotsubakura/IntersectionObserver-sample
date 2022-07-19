474d691e03fbcd24637798f932c7320a2c8b7e9a
Rohito Tsubakura <rohitotsubakura@gmail.com>
2022-07-19 09:37:44 +0900

    modify: setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,5 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    target: 'static',
-    app: {
-        cdnURL: '/<reponame>/'
-    }
+    target: 'static'
 })
