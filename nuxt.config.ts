352e7ffdb82a949e77bca733b985e20435692329
Rohito Tsubakura <rohitotsubakura@gmail.com>
2022-07-19 09:39:12 +0900

    modify: setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    target: 'static'
+    target: 'static',
+    app: {
+        baseURL: '/<repository-name>'
+    }
 })
